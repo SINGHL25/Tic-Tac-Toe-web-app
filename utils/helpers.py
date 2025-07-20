07a020b46a63bd3a2a8ca339feb9b591a0fa1262
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-20 22:12:17 +1000

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,14 +1,14 @@
 # utils/helper.py
-import streamlit as st
-import time
-import random
+# import streamlit as st
+# import time
+# import random
 
 
-def show_winner_celebration(player):
-    st.balloons()
-    st.markdown("## 🎉 Congratulations!")
-    st.image(player["image"], width=200) if player["image"] else None
-    for _ in range(10):
-        st.markdown(f"### 🎆 {random.choice(['🔥', '✨', '💥', '🎇', '🎆'])} Winner Party 🎆")
-        time.sleep(0.2)
+# def show_winner_celebration(player):
+#     st.balloons()
+#     st.markdown("## 🎉 Congratulations!")
+#     st.image(player["image"], width=200) if player["image"] else None
+#     for _ in range(10):
+#         st.markdown(f"### 🎆 {random.choice(['🔥', '✨', '💥', '🎇', '🎆'])} Winner Party 🎆")
+#         time.sleep(0.2)
 
